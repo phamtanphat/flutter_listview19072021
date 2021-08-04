6e3cbab09beaa8f944e937ecefa1efe692c81704
phamtanphat <phatdroid94@gmail.com>
2021-08-04 18:12:35 +0700

    task model

---
+++ lib/model/task.dart
@@ -0,0 +1,7 @@
+class Task{
+  late int id;
+  late String name;
+  late String description;
+
+  Task({required this.id , required this.name , required this.description});
+}
--- lib/page/list_page.dart
+++ lib/page/list_page.dart
@@ -1,16 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ListPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ListPageContainer();
   }
 }
 
-
 class ListPageContainer extends StatefulWidget {
-
   @override
   _ListPageContainerState createState() => _ListPageContainerState();
 }
@@ -23,9 +21,18 @@ class _ListPageContainerState extends State<ListPageContainer> {
         title: Text("List Task"),
       ),
       body: Container(
-        child: Text("Nothing"),
+          child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context ,index){
+                  return ListTile(
+                    title: Text("Item $index"),
+                    subtitle: Text("Description "),
+                    leading: Text(index.toString()),
+                    trailing: Icon(Icons.delete),
+                  );
+              }
+          )
       ),
     );
   }
 }
-
